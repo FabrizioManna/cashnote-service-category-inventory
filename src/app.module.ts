b8e3b5149785876bd78c80d07834a4dd049a3bbd
mannafabrizio83 <mannafabrizio83@gmail.com>
2023-04-19 22:22:55 +0200

    Init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { CategoryInventoryModule } from './category-inventory/category-inventory.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule, ApolloDriverConfig } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true
+    }),
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: process.env.GRAPHQL_PLAY === 'true' ? true : false || true,
+    }),
+
+    CategoryInventoryModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
